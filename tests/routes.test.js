ef2ada289bf89e48cc85894488e463934fe7dd41
Thomas Harper <thomasharper@CIC00966.local>
2019-10-14 13:50:31 +0100

    add integration tests for api

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const { spawn } = require("child_process");
 
 let slsOfflineProcess;
+let port = 4000;
 
 beforeAll(done => {
   jest.setTimeout(20000);
@@ -9,29 +10,60 @@ beforeAll(done => {
     if (err) {
       return done(err);
     }
-    console.log("its started");
     done();
   });
 });
 
-it("checks for a 200 response", async () => {
-  const response = await request("http://localhost:3000").get("/workouts");
+it("returns all workouts, in expected format", async () => {
+  const response = await request(`http://localhost:${port}`).get(`/workouts`);
   expect(response.statusCode).toBe(200);
+  expect(response.body).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        id: expect.any(Number),
+        title: expect.any(String),
+        whatdescription: expect.any(String),
+        dodescription: expect.any(String),
+        workimage: expect.any(String),
+        workouttype: expect.any(String),
+        workoutdifficulty: expect.any(String)
+      })
+    ])
+  );
+});
+
+it("returns a a single, beginner, kettlebell workout", async () => {
+  const response = await request(`http://localhost:${port}`).get(
+    `/workouts/beginner/kettlebell/1`
+  );
+  expect(response.statusCode).toBe(200);
+  expect(response.body.length).toBe(1);
+  expect(response.body).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        workouttype: "kettlebell",
+        workoutdifficulty: "beginner"
+      })
+    ])
+  );
+});
+
+it("returns multiple workouts", async () => {
+  const response = await request(`http://localhost:${port}`).get(
+    `/workouts/beginner/coding/3`
+  );
+  expect(response.body.length).toBe(3);
 });
 
 afterAll(() => {
-  console.log("closing down serverless");
   stopSlsOffline();
-  console.log("night night");
 });
 
 const startSlsOffline = done => {
-  slsOfflineProcess = spawn("sls", ["offline", "start", "--port", 3000]);
-  console.log(`serverless started with PID: ${slsOfflineProcess.pid}`);
+  slsOfflineProcess = spawn("sls", ["offline", "start", "--port", port]);
 
   slsOfflineProcess.stdout.on("data", data => {
-    if (data.includes("Offline [HTTP] listening on http://localhost:3000")) {
-      console.log(data.toString().trim());
+    if (data.includes(`Offline [HTTP] listening on http://localhost:${port}`)) {
       done();
     }
   });
@@ -44,5 +76,4 @@ const startSlsOffline = done => {
 
 const stopSlsOffline = () => {
   slsOfflineProcess.kill();
-  console.log("serverless offline stopped");
 };
